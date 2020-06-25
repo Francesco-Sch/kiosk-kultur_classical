6623f4b20fd7b6ab714d48ac6104956a8d283552
Francesco Scheffczyk <francessco.scheffczyk@gmail.com>
2020-06-25 19:28:19 +0200

    Let information boxes appear on hover

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,28 @@
+// Activates slider
 var mySwiper = new Swiper ('.swiper-container', {
     slidesPerView: 1.4,
     spaceBetween: 30,
     freeMode: true,
     slidesOffsetBefore: 700,
     setWrapperSize: true
+});
+
+// Shows information boxes on hover
+const informationIcons = document.querySelectorAll('.icon');
+
+function showInformation(item) {
+    item.nextElementSibling.classList.add('show');
+}
+
+function hideInformation(item) {
+    item.nextElementSibling.classList.remove('show');
+}
+
+informationIcons.forEach( (icon) => {
+    icon.addEventListener('mouseenter', () => {
+        showInformation(icon)
+    });
+    icon.addEventListener('mouseleave', () => {
+        hideInformation(icon)
+    });
 })
